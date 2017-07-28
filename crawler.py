83b935aad3178556ac0ae089574607add0e6e79a
Léu Jesus <Leu@MacBook-Air-de-Leu.local>
2017-07-28 00:39:29 -0300

    Adicionando a parte de banco de dados

--- crawler.py
+++ crawler.py
@@ -1,33 +1,57 @@
 import requests
 from bs4 import BeautifulSoup
-
-
-
-class Materia(object):
-    descricao = None
-    area_de_conhecimento = None
-    duracao = None
-
-    def create_sql(self):
-        return "insert into table values ('{}','{}','{}')".format(self.descricao.replace('\n', '').replace('\r','').strip(), self.area_de_conhecimento.strip(), self.duracao.strip())
-
+from database import Curso, Unidade
+from database import session
 
 r = requests.get('http://www.pucminas.br/Pos-Graduacao/IEC/modalidades/latusensu/Paginas/Especializa%C3%A7%C3%A3o%20e%20Master.aspx')
 
-
 soup = BeautifulSoup(r.text, 'html.parser')
 
+'''
+unidades = soup.select('h3.puc-wp-encontre-seu-curso-unidade')
 
-#Obter links para a materia
-links = soup.select('a.puc-wp-encontre-seu-curso-link-curso')
+for i in unidades:
+    u = Unidade()
+    u.unidade = i.text
+    session.add(u)
+
+session.commit()
+'''
+
+cursos = soup.select('a.puc-wp-encontre-seu-curso-link-curso')
+
+for c in cursos:
+    print (c.text)
+
+    request_curso = requests.get(c['href'])
+
+    sc = BeautifulSoup(request_curso.text, 'html.parser')
+
+
+
+    div = sc.find(id="ctl00_PlaceHolderMain_ctl08__ControlWrapper_RichHtmlField")
+    p = div.select('p')[0].text
+
+    print (p)
+
+    '''
+    div = sc.find('div', {'class': 'puc-pl-pos-graduacao-conteudo-area-conhecimento'})
+    children = div.findChildren()
+    for child in children:
+        print (child)
+    '''
+
+
+
+'''
+links = 
 
 for link in links:
     materia = Materia()
     request_para_pagina_curso = requests.get(link['href'])
 
-    soup_curso = BeautifulSoup(request_para_pagina_curso.text, 'html.parser')
+    
     materia.descricao = soup_curso.select_one('.puc-wp-curso-outras-unidades').text
     materia.area_de_conhecimento = soup_curso.select_one('.puc-pl-pos-graduacao-conteudo').text
     materia.duracao = soup_curso.select_one('#ctl00_PlaceHolderMain_ctl11__ControlWrapper_RichHtmlField').text
-
-    print(materia.create_sql())
+'''
---
+++ database.py
@@ -0,0 +1,47 @@
+from sqlalchemy import *
+from sqlalchemy.orm import sessionmaker
+from sqlalchemy.ext.declarative import declarative_base
+
+engine = create_engine('sqlite://////Users/Leu/Projetos/scrp-andre/Sample.db')
+Session = sessionmaker(bind=engine)
+session = Session()
+
+Base = declarative_base()
+
+'''
+class User(Base):
+    __tablename__ = 'users'
+    user_id = Column(Integer(), primary_key = True)
+    user_name = Column(String(80), unique=True)
+
+
+
+u = User()
+u.user_name = 'user2'
+session.add(u)
+session.commit()
+'''
+
+class Curso(Base):
+	__tablename__ = 'cursos'
+	id = Column(Integer(), primary_key = True)
+	nome_curso = Column(String(255),)
+	area_conhecimento = Column(String(4000))
+	descricao = Column(String(4000))
+	area_conhecimento_cnpq = Column(String(4000))
+	importante = Column(String(4000))
+	objetivos = Column(String(4000))
+	unidade_id = Column(Integer(), ForeignKey('unidades.id'), nullable=False)
+
+class Unidade(Base):
+	__tablename__ = 'unidades'
+	id = Column(Integer(), primary_key = True)
+	unidade = Column(String(80), unique=True)
+
+
+Base.metadata.create_all(engine)
+
+
+
+	
+
