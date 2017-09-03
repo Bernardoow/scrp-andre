255a14943e5899f2e506fd6ccb92e54574ddc9b5
Andre <dedeco@gmail.com>
2017-09-03 02:52:43 -0300

    Finalmente crawler funcionando

---
+++ captura.py
@@ -0,0 +1,116 @@
+﻿#!/usr/bin/env python
+# -*- coding: utf-8 -*-
+
+import requests
+import os
+import re
+
+from selenium import webdriver
+from selenium.webdriver.common.keys import Keys
+from bs4 import BeautifulSoup
+from database import Unidade, Curso, AreaConhecimento, Objetivo, Programa
+from database import Base, engine, session
+
+def init_browser():
+	driver = "/Users/dedeco/Apps/chromedriver"
+	os.environ["webdriver.chrome.driver"] = driver
+	b = webdriver.Chrome(driver)	
+	return b
+
+def captura(url):
+
+	b = init_browser()
+
+	r = requests.get(url)
+	soup = BeautifulSoup(r.text, 'html.parser')
+	links = soup.select('a.puc-wp-encontre-seu-curso-link-curso')
+	unidades = soup.select('h3.puc-wp-encontre-seu-curso-unidade')
+
+	for i in unidades:
+		u = Unidade()
+		u.unidade = i.text
+		session.add(u)
+		session.flush()
+	
+	for l in links:
+		
+		link = l['href']
+
+		b.get(l['href'])
+		b.implicitly_wait(2)
+		sp_curso = BeautifulSoup(b.page_source, 'html.parser')
+
+		curso = sp_curso.select_one('h2.puc-pl-titulo-pg').text.strip()
+
+		ac = sp_curso.select_one('.puc-pl-pos-graduacao-conteudo-area-conhecimento ul') 
+
+		conhecimentos = []
+		for litag in ac.find_all('li'):
+			conhecimentos.append(litag.text)
+
+		div = sp_curso.find(id="ctl00_PlaceHolderMain_ctl08__ControlWrapper_RichHtmlField")
+		texto = ''
+
+		ptags = div.find_all("p")
+		for tag in ptags:
+			if len(tag.text) > 20:
+				texto += '\r' + tag.text
+
+		spantags = div.find_all("span")
+		for tag in spantags:
+			if len(tag.text) > 20:
+				texto += '\r' + tag.text
+
+		divs = div.find_all("div")
+		for tag in divs:
+			if len(tag.text) > 20:
+				texto += "\r" + tag.text
+
+		m = re.search('(.*)\\s*(?=Área)', texto)
+		descricao = m.group(0)
+
+		c = Curso()
+		c.nome_curso = curso
+		c.descricao = descricao
+		c.texto = texto
+		c.link = link
+		c.unidade_id = 1
+
+		session.add(c)
+		session.flush()
+
+		ultags = div.find_all("ul")
+
+		obj = []
+		for litag in ultags[0].find_all('li'):
+			obj.append(litag.text)
+
+		for i in obj:
+			o = Objetivo()
+			o.objetivo = i
+			o.curso_id = c.id
+			session.add(o)
+
+		programa = []
+		try:
+			for litag in ultags[1].find_all('li'):
+				programa.append(litag.text)
+		except IndexError:
+			pass
+
+		for i in programa:
+			p = Programa()
+			p.programa = i
+			p.curso_id = c.id
+			session.add(p)
+
+		session.commit()
+
+	b.quit()
+
+if __name__ == "__main__":
+	Base.metadata.drop_all(engine)
+	Base.metadata.create_all(engine)
+	url = "http://www.pucminas.br/Pos-Graduacao/IEC/modalidades/latusensu/Paginas/Especializa%C3%A7%C3%A3o%20e%20Master.aspx"
+	captura(url)
+
--- database.py
+++ database.py
@@ -2,44 +2,43 @@ from sqlalchemy import *
 from sqlalchemy.orm import sessionmaker
 from sqlalchemy.ext.declarative import declarative_base
 
-engine = create_engine('sqlite://////Users/Leu/Projetos/scrp-andre/Sample.db')
+engine = create_engine('sqlite:///////Users/dedeco/Projetos/scrp-andre/Sample.db')
 Session = sessionmaker(bind=engine)
 session = Session()
 
 Base = declarative_base()
 
-'''
-class User(Base):
-    __tablename__ = 'users'
-    user_id = Column(Integer(), primary_key = True)
-    user_name = Column(String(80), unique=True)
-
-
-
-u = User()
-u.user_name = 'user2'
-session.add(u)
-session.commit()
-'''
-
 class Curso(Base):
 	__tablename__ = 'cursos'
 	id = Column(Integer(), primary_key = True)
 	nome_curso = Column(String(255),)
-	area_conhecimento = Column(String(4000))
 	descricao = Column(String(4000))
-	area_conhecimento_cnpq = Column(String(4000))
-	importante = Column(String(4000))
-	objetivos = Column(String(4000))
+	texto = Column(String(4000))
+	link = Column(String(255))
 	unidade_id = Column(Integer(), ForeignKey('unidades.id'), nullable=False)
+
+class AreaConhecimento(Base):
+	__tablename__ = 'areas_conhecimentos'
+	id = Column(Integer(), primary_key = True)
+	area_conhecimento = Column(String(255))
+	curso_id = Column(Integer(), ForeignKey('cursos.id'), nullable=False)
+
+class Objetivo(Base):
+	__tablename__ = 'objetivos'
+	id = Column(Integer(), primary_key = True)
+	objetivo = Column(String(255))
+	curso_id = Column(Integer(), ForeignKey('cursos.id'), nullable=False)
+
+class Programa(Base):
+	__tablename__ = 'programas'
+	id = Column(Integer(), primary_key = True)
+	programa = Column(String(255))
+	curso_id = Column(Integer(), ForeignKey('cursos.id'), nullable=False)
 
 class Unidade(Base):
 	__tablename__ = 'unidades'
 	id = Column(Integer(), primary_key = True)
 	unidade = Column(String(80), unique=True)
-
-
-#Base.metadata.create_all(engine)
 
 
 
